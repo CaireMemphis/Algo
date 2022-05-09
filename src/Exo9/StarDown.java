48bdee118111132ff1102176f3aa6ca2832afd02
Thomas Screve <thomas-scrve@laposte.net>
2022-05-09 16:41:02 +0200

    exo d'algo

--- src/Exo9/StarDown.java
+++ src/Exo9/StarDown.java
@@ -19,12 +19,9 @@ public class StarDown {
             calcul += "*";
             System.out.println(calcul);
         }
-
-
-
         for (int i = nb; i > 1; i--) {
             bn += 1;
             System.out.println(calcul.substring(bn));
-
         }
-}}
+    }
+}
