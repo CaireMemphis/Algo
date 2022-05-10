2000e28957df62e2116efdfb16801df770305159
Thomas Screve <thomas-scrve@laposte.net>
2022-05-10 16:49:15 +0200

    exo d'algo

--- src/ExoAPart/fizzbuzz.java
+++ src/ExoAPart/fizzbuzz.java
@@ -1,9 +1,13 @@
 package ExoAPart;
+import java.util.Collections;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class fizzbuzz {
     static int nb;
     public static void main(String[] args) {
-        int rv = 0;
+       /* int rv = 0;
       do {
           Scanner scanChif = new Scanner(System.in);
           System.out.print(" chiffre : ");
@@ -18,6 +22,10 @@ public class fizzbuzz {
           }
           System.out.println(result);
       }
-      while(rv == 0);
+      while(rv == 0);*/
+
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (i % 7 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
     }
 }
