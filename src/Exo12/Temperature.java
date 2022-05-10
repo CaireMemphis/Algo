c7aaf6cab913041f5c3cbd2fe61c8a8d52a121e3
Thomas Screve <thomas-scrve@laposte.net>
2022-05-10 14:42:07 +0200

    exo d'algo

--- src/Exo12/Temperature.java
+++ src/Exo12/Temperature.java
@@ -2,40 +2,40 @@ package Exo12;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 public class Temperature {
     public static void main(String[] args) {
-     celcius();
+        int[] essai = {2,-1,8,5,14,-10,-5,21,-2,1};
+        afficher(essai);
+     celcius(essai);
     }
-    public static void celcius(){
-        ArrayList<Integer> fahrenheit = new ArrayList<>();
-        fahrenheit.add(12);
-        fahrenheit.add(25);
-        fahrenheit.add(5);
-        fahrenheit.add(7);
-        fahrenheit.add(6);
-        fahrenheit.add(-5);
-        fahrenheit.add(-10);
-        fahrenheit.add(27);
-        fahrenheit.add(-1);
-        fahrenheit.add(2);
-        fahrenheit.add(-3);
-        fahrenheit.add(1);
-        fahrenheit.add(0);
+    public static void celcius(int [] fahrenheit){
+        int tMinPlus =0;
+        int tMinMoins= 0;
 
-        int tMin = 0;
-        int tMinPlus = (int) Collections.max(fahrenheit);
-        int tMinMoins= (int) Collections.min(fahrenheit);
+        for (int i = 0; i < fahrenheit.length; i++) {
+            if (fahrenheit[i]>tMinPlus){
+                tMinPlus = fahrenheit[i];
+            }
+            if (fahrenheit[i]<tMinMoins){
+                tMinMoins = fahrenheit[i];
+            }
+        }
+        for (int i = 0; i < fahrenheit.length; i++) {
 
-        for (int i = 0; i < fahrenheit.size(); i++) {
-            if (fahrenheit.get(i) <=0){
-            if (fahrenheit.get(i) >=tMinMoins) {
-                tMinMoins = fahrenheit.get(i);
-            }}
-            if (fahrenheit.get(i) >=0){
-                if (fahrenheit.get(i) <=tMinPlus){
-                    tMinPlus = fahrenheit.get(i);
+            if (fahrenheit[i] <=0){
+
+            if (fahrenheit[i] >=tMinMoins) {
+
+                tMinMoins = fahrenheit[i];
+                }
+            }
+            if (fahrenheit[i] >=0){
+
+                if (fahrenheit[i] <=tMinPlus){
+                    tMinPlus = fahrenheit[i];
                 }
             }
         }
@@ -47,10 +47,18 @@ public class Temperature {
         for (int i = 0; i > tMinMoins; i--) {
             InterMoins+=1;
         }
-        System.out.println(fahrenheit);
+
 if (InterMoins<=InterPlus){
     System.out.println("La température la plus proche de 0°C est " + tMinMoins +"°C.");
 }else {
         System.out.println("La température la plus proche de 0°C est " + tMinPlus +"°C.");
     }
-}}
+}
+
+public static void afficher(int[] essai) {
+    for (int i = 0; i < essai.length; i++) {
+        System.out.print(essai[i]+" ");
+    }
+    System.out.println();
+}
+}
---
+++ src/Exo13/Tri.java
@@ -0,0 +1,53 @@
+package Exo13;
+
+import java.util.ArrayList;
+
+public class Tri {
+    public static void main(String[] args) {
+
+        int[] essai = {2, 5, 6, 4, 9, 6, 4, 1, 3, 7, 9, 12, 5, 14, 84};
+        afficher(essai);
+        triageBulle(essai);
+        //triageSelection(essai);
+        afficher(essai);
+    }
+
+    public static void triageBulle(int[] essai) {
+
+        int taille = essai.length;
+        int depla = 0;
+        for (int i = 0; i < taille; i++) {
+            for (int j = 1; j < (taille - i); j++) {
+                if (essai[j - 1] > essai[j]) {
+                    depla = essai[j - 1];
+                    essai[j - 1] = essai[j];
+                    essai[j] = depla;
+                }
+            }
+        }
+    }
+
+    public static void triageSelection(int[] essai) {
+        int taille = essai.length;
+        int index = 0;
+
+        for (int i = 0; i < taille - 1; i++) {
+            int petit = i;
+            for (int j = i + 1; j < (taille); j++) {
+                if (essai[j] < essai[petit]) {
+                    petit = j;
+                }
+            }
+            int smallNb = essai[petit];
+            essai[petit] = essai[i];
+            essai[i] = smallNb;
+        }
+    }
+
+    static void afficher(int[] tab) {
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + " ");
+        }
+        System.out.println();
+    }
+}
