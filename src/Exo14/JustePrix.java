ab1335c2d59adcc2feb85d0d15edc732b811475a
Thomas Screve <thomas-scrve@laposte.net>
2022-05-11 09:55:24 +0200

    exo d'algo

---
+++ src/Exo14/JustePrix.java
@@ -0,0 +1,55 @@
+package Exo14;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JustePrix {
+    static int prix;
+    static int tentative;
+
+    public static void main(String[] args) {
+
+        parametres();
+        trouver();
+    }
+
+    public static void trouver() {
+        Scanner scan = new Scanner(System.in);
+        int proposition = 0;
+        for (int i = 1; i <= tentative; i++) {
+            System.out.print("Proposition " + i + "/" + tentative + " : ");
+            proposition = scan.nextInt();
+            System.out.println();
+
+            if (proposition < prix) {
+                System.out.println("c'est plus");
+            }
+            if (proposition > prix) {
+                System.out.println("c'est moins !");
+            }
+            if (proposition == prix) {
+                System.out.println("C'est gagné !!!!!!!!!!!");
+                System.out.println("Juste Prix : "+prix+" €");
+                break;
+            }
+        }
+        if (proposition != prix) {
+            System.out.println("Essais ecoulés ... Perdu !!!");
+            System.out.println("Juste Prix : "+prix+" €");
+        }
+
+    }
+
+    public static void parametres() {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("nb de tentatives ?");
+        tentative = sc.nextInt();
+        System.out.println("minimum ?");
+        int min = sc.nextInt();
+        System.out.println("maximum ?");
+        int max = sc.nextInt();
+        prix = min + random.nextInt(max - min);
+    }
+}
--- src/ExoAPart/fizzbuzz.java
+++ src/ExoAPart/fizzbuzz.java
@@ -1,31 +1,25 @@
 package ExoAPart;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class fizzbuzz {
-    static int nb;
-    public static void main(String[] args) {
-       /* int rv = 0;
-      do {
-          Scanner scanChif = new Scanner(System.in);
-          System.out.print(" chiffre : ");
-          nb = scanChif.nextInt();
-          String result = "";
-          if (nb % 3 == 0) {
-              result = "Fizz";
-          } if (nb % 5 == 0 && nb % 3 != 0) {
-              result = "Buzz";
-          } if (nb % 5 == 0 && nb % 3 == 0) {
-              result = "FizzBuzz";
-          }
-          System.out.println(result);
-      }
-      while(rv == 0);*/
+    static int nb = 1;
 
-        IntStream.rangeClosed(1, 100)
-                .mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (i % 7 == 0 ? "Buzz" : i))
-                .forEach(System.out::println);
+    public static void main(String[] args) {
+        int rv = 0;
+        do {
+            String result = "...";
+            if (nb % 3 == 0) {
+                result = "Fizz";
+            }
+            if (nb % 5 == 0 && nb % 3 != 0) {
+                result = "Buzz";
+            }
+            if (nb % 5 == 0 && nb % 3 == 0) {
+                result = "FizzBuzz";
+            }
+            System.out.println(nb + " : " + result);
+            nb += 1;
+        }
+        while (nb <= 50);
+        System.out.println("Fin");
     }
 }
